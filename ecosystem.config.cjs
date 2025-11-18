aad98d1fb94043c1c5d2b63c66840883546e40f7
Kojaehyeong <jaehyeong1586@gmail.com>
2025-11-18 18:33:46 +0900

    server.mjs 설정

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "manna-crawling",
+  exec_mode: "fork",
+  script: "./server.mjs",
+  autorestart: true,
+  watch: false,
+  env_prod: {
+    NODE_ENV: "prod",
+  },
+  env_test: {
+    NODE_ENV: "test",
+  },
+  env_development: {
+    NODE_ENV: "development",
+  },
+};
--- server.mjs
+++ server.mjs
@@ -1,53 +1,54 @@
-import express from 'express';
-import * as path from 'path';
-import history from 'connect-history-api-fallback';
-import compression from 'compression';
-import ip from 'ip';
-import dotenv from 'dotenv';
+import express from "express";
+import * as path from "path";
+import history from "connect-history-api-fallback";
+import compression from "compression";
+import ip from "ip";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const DIST_PATH =
-	process.env.NODE_ENV === 'dev'
-		? path.join(__dirname, 'dist')
-		: path.join(__dirname, 'apps', 'manna-crawling', 'dist');
+  process.env.NODE_ENV === "development"
+    ? path.join(__dirname, "dist")
+    : path.join(__dirname, "manna-crawling", "dist");
+
 const DIST_INDEX_PATH =
-	process.env.NODE_ENV === 'dev'
-		? path.join(__dirname, 'dist', 'index.html')
-		: path.join(__dirname, 'apps',  'manna-crawling', 'dist', 'index.html');
+  process.env.NODE_ENV === "development"
+    ? path.join(__dirname, "dist", "index.html")
+    : path.join(__dirname, "manna-crawling", "dist", "index.html");
 
 const app = express();
 
 app.use(compression());
 app.use(
-	history({
-		rewrites: [
-			{
-				from: /^\/(?!assets\/).*/, // /assets/로 시작하지 않는 모든 요청만 index.html로 rewrite
-				to: '/index.html',
-			},
-		],
-	}),
+  history({
+    rewrites: [
+      {
+        from: /^\/(?!assets\/).*/, // /assets/로 시작하지 않는 모든 요청만 index.html로 rewrite
+        to: "/index.html",
+      },
+    ],
+  })
 );
 app.use(
-	express.static(DIST_PATH, {
-		maxAge: '7d', // 캐싱 적용
-		etag: true,
-		index: false,
-	}),
+  express.static(DIST_PATH, {
+    maxAge: "7d", // 캐싱 적용
+    etag: true,
+    index: false,
+  })
 );
 
 app.get(/.*/, (req, res) => {
-	res.setHeader('Cache-Control', 'no-store');
-	res.setHeader('max-age', '60');
-	res.sendFile(DIST_INDEX_PATH);
+  res.setHeader("Cache-Control", "no-store");
+  res.setHeader("max-age", "60");
+  res.sendFile(DIST_INDEX_PATH);
 });
 
-const PORT = 9001;
+const PORT = 4100;
 
 app.listen(PORT, () => {
-	console.log(
-		`Server running at ${process.env.NODE_ENV === 'dev' ? `http://localhost:${PORT}` : `http://${ip.address()}:${PORT}`}`,
-	);
+  console.log(
+    `Server running at ${process.env.NODE_ENV === "development" ? `http://localhost:${PORT}` : `http://${ip.address()}:${PORT}`}`
+  );
 });
